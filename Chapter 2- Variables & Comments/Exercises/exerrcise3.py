d1feea8e90f56d5859a28d28b8a3d0135679ae2c
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-10-16 09:06:39 +0400

    Chapter2_exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exerrcise3.py
@@ -0,0 +1,20 @@
+name = ("\tJohn Lloyd\n")
+
+print("\n (n)lstrip:")
+print(name.lstrip())
+
+print("\n (n)rstrip:")
+print(name.rstrip())
+
+print("\n (n)strip:")
+print(name.strip())
+
+
+print("\t (t)lstrip:")
+print(name.lstrip())
+
+print("\t (t)rstrip:")
+print(name.rstrip())
+
+print("\t (t)strip:")
+print(name.strip())
