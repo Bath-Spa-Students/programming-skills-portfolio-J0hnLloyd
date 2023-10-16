09686aa12e65d2bc334cd1401a7a45d19d55c513
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-10-16 09:07:36 +0400

    practice.py

---
+++ Practice/PwacticeUwU.py
@@ -0,0 +1,34 @@
+Student = (input ("Student's Name: "))
+
+gender = (input ("Gender: "))
+
+age = (input ("Age: "))
+
+Contact_Number = (input ("Contact/Mobile Number: "))
+
+email = (input ("Student's Email: "))
+
+father = (input ("Father's Name:  "))
+
+father_email = (input ("Father's Email "))
+
+father_number = (input ("Father's Contact Number: "))
+
+mother = (input ("Mother's Name: "))
+
+mother_email = (input ("Mother's Email: "))
+
+mother_number = (input ("Mother's Contact Number: "))
+
+
+print (Student)
+print (gender)
+print (age)
+print (Contact_Number)
+print (email)
+print (father)
+print (father_email)
+print (father_number)
+print (mother)
+print (mother_email)
+print (mother_number)
