b26391dcae6a9ae259c4511921f0817ad4049f63
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-10-16 09:07:09 +0400

    Chapter2_exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/exerrcise5.py
@@ -0,0 +1,9 @@
+Money = ("Money = 50")
+USB_Val = ("USB Value = 6")
+print (Money)
+print (USB_Val)
+
+UsB = "USB Bought = "
+Saved = "Amoung Saved = "
+print (UsB + str(int(50/6)))
+print (Saved + str(int(50%6)))
