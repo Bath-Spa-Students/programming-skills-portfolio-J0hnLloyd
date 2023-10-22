8d590a087ecde04c28e6e7f952a1d7d9d29c46ae
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-10-22 21:31:26 +0400

    practice5.py

---
+++ Chapter 1- Getting Started with Python/(Class Exercises)/exercise5.py
@@ -0,0 +1,10 @@
+#Q5: Write four strings in different variables and print the output as one string. (String Concatenation)
+
+string1 = ("John Lloyd, ")
+string2 = ("Encenarez, ")
+string3 = ("Madridano, ")
+string4 = ("JL ")
+
+result = (string1 + string2 + string3 + string4)
+
+print(result)
