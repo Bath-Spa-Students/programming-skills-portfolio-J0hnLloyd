21ff205937c852f864d70823e4cb4f9394e82ea6
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:55:05 +0400

    Chp3_Exercise#2.py

---
+++ Chapter 3- Data Structures/Exercises/Chp3_Exercise#2.py
@@ -0,0 +1,22 @@
+# Start with the list you used in Exercise 1, but instead of just...
+
+# printing each person’s name, print a message to them. The text of each message should be the same, but each message should be...
+
+# personalized with the person’s name...
+
+names = ['Isaac' , 'Rafaelx2' , 'Ismail' , 'Yyan' , 'Paul']
+
+Message = ("Dear " + names[0] + ", you're a cool friend")
+print (Message)
+
+Message = (names[1] + " you guys have the same haha")
+print (Message)
+
+Message = ("Hey " + names[2] + ", why always serious")
+print (Message)
+
+Message = (names[3] + " Habibi my guy, Meow")
+print (Message)
+
+Message = (names[4] + " you funny")
+print (Message)
