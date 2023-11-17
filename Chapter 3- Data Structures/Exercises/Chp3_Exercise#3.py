3639b92c29bb26644d06d7f5ba217ca2d7c0d4c1
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:55:12 +0400

    Chp3_Exercise#3.py

---
+++ Chapter 3- Data Structures/Exercises/Chp3_Exercise#3.py
@@ -0,0 +1,20 @@
+# List of Transportations...
+MOT = ['Family/Personal Car' , 'Bus' , 'Taxi' , 'Metro Train']
+
+# Transportations & Messages
+print("\n")
+Transpo = (MOT[0] + ", Is by far my favourite out of the entire list, mainlyl because it's the most convenient out of everything else in this list.")
+print (Transpo)
+
+print("\n")
+Transpo = (MOT[1] + ". They're annoying and it smells alot inside, with the exception of the buses around my area, it's still not the best but very situational and it's cheap.")
+print (Transpo)
+
+print("\n")
+Transpo = (MOT[2] + ", Its expensive but conveninet in certain situations plus its easily accesible")
+print (Transpo)
+
+print("\n")
+Transpo = ("I really like taking the " + MOT[3] + ", It's cheap and it's the fastest out of any transportation i take with the exception of our family car")
+print (Transpo)
+
