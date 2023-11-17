1ea7632d29290f46dc0e04f103150c1720b97070
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:55:17 +0400

    Chp3_Exercise#4.py

---
+++ Chapter 3- Data Structures/Exercises/Chp3_Exercise#4.py
@@ -0,0 +1,13 @@
+# Guest List...
+Guests = ['Dhen Mark' , 'Ghian Mark' , 'Johann Eroa']
+
+# Invitations & Messages...
+Invitation_Msg = ("\nHey " + Guests[0] + ", Please join us for dinner.")
+print (Invitation_Msg)
+
+Invitation_Msg = ("\nDear " + Guests[1] + ", Please join us for dinner.")
+print (Invitation_Msg)
+
+Invitation_Msg = ("\nHello " + Guests[2] + ", Please join us for dinner.")
+print (Invitation_Msg)
+
