7aac742faaf6da9c87b446d670c76b06dba8ad56
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:55:37 +0400

    Chp3_Exercise#7.py

---
+++ Chapter 3- Data Structures/Exercises/Chp3_Exercise#7.py
@@ -0,0 +1,36 @@
+Locations = ['Japan' , 'Switzerland' , 'New Zealand' , 'Singapore']
+
+# Original Order...
+print ("Original Order:")
+print (Locations)
+
+# Alphabetical Order...
+print ("\nAlphabetical Order:")
+print (sorted(Locations))
+
+print ("\nOriginal Order:")
+print (Locations)
+
+# Reverse Alphabetical Order...
+print("\nReversed Alphabetical Order:")
+print (sorted(Locations, reverse=True))
+
+print ("\nOriginal Order:")
+print (Locations)
+
+# Reverse Order...
+
+print("\nReversed Order")
+Locations.reverse()
+print(Locations)
+
+print ("\nOriginal Order:")
+print (Locations)
+
+# Alphabitcal and Reversed Alphabetical again...
+
+print ("\nAlphabetical Order:")
+print (sorted(Locations))
+
+print("\nReversed Alphabetical Order:")
+print (sorted(Locations, reverse=True))
