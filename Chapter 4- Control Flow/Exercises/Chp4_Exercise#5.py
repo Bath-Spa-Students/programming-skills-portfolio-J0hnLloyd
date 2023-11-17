62cc6a7857d6cdca2f3b8224917e46bec63ea73d
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:56:20 +0400

    Chp4_Exercise#5.py

---
+++ Chapter 4- Control Flow/Exercises/Chp4_Exercise#5.py
@@ -0,0 +1,13 @@
+# List of Favourite Fruits...
+favorite_fruits = ['Apples', 'Bananas', 'Oranges']
+
+if 'Bananas' in favorite_fruits:
+    print("You really like Bananas!") # If the fruit is in your list, the if block (Bananas!)
+if 'Apples' in favorite_fruits:
+    print("You really like Apples!") # If the fruit is in your list, the if block (Apples!)
+if 'Oranges' in favorite_fruits:
+    print("You really like Oranges!") # If the fruit is in your list, the if block (Oranges!)
+if 'Coconuts' in favorite_fruits:
+    print("You really like Coconuts!")
+if 'Mangoes' in favorite_fruits:
+    print("You really like Mangoes!")
