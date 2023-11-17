ad729880ab3dcf3231f59ec70965871909b57f97
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:57:46 +0400

    Chp7_Exercise#2.py

---
+++ Chapter 7- Functions/Exercises/Chp7_Exercise#2.py
@@ -0,0 +1,6 @@
+# Use def to define favourite_book then add a message... 
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+# Call the function with the book title to display the statement...
+favorite_book("Alice in Wonderland")
