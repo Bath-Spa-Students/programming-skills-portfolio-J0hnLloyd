468fdde8fc6d10086d10656dc12a695f0a627ebe
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:56:09 +0400

    Chp4_Exercise#3.py

---
+++ Chapter 4- Control Flow/Exercises/Chp4_Exercise#3.py
@@ -0,0 +1,25 @@
+# Green...
+AC = ("Green")
+print("\n")
+if AC == "Green":
+    print("You just earned 5 points")
+else:
+    print("You just earned 10 points!")
+
+# Yellow...
+AC = ("Yellow")
+print("\n")
+if AC == "Green":
+    print("You just earned 5 points")
+else:
+    print("You just earned 10 points")
+
+# Red
+AC = ("Red")
+print("\n")
+if AC == "Green":
+    print("You just earned 5 points")
+elif AC == "Yellow":
+    print("You just earned 10 points")
+else:
+    print("You just earned 15 points")
