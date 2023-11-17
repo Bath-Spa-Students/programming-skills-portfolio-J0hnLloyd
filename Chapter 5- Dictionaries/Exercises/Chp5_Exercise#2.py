384f1660a610f5e3d919d1d378b3725b82427af9
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:56:42 +0400

    Chp5_Exercise#2.py

---
+++ Chapter 5- Dictionaries/Exercises/Chp5_Exercise#2.py
@@ -0,0 +1,23 @@
+# Glossary...
+Programming_words = {'String' : 'Stores Text & Characters',
+                     'Variable' : 'Containers of Data Values',
+                     'Comment' : 'Uses the Character # to comment',
+                     'Lists' : 'Uses brackets to create lists', 
+                     'Else & if statements' : 'Run only when a certain condition is met.',
+                     }
+Pw = 'String'
+print("\n" + Pw.title() + ": " + Programming_words[Pw])
+
+Pw = 'Variable'
+print("\n" + Pw.title() + ": " + Programming_words[Pw])
+
+Pw = 'Comment'
+print("\n" + Pw.title() + ": " + Programming_words[Pw])
+
+Pw = 'Lists'
+print("\n" + Pw.title() + ": " + Programming_words[Pw])
+
+Pw = 'Else & if statements'
+print("\n" + Pw.title() + ": " + Programming_words[Pw])
+
+
