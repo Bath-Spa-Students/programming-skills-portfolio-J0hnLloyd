4926d4380d75b7a06d3b6b5b2aa1a4bffa5f64f7
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:58:00 +0400

    Chp7_Exercise#5.py

---
+++ Chapter 7- Functions/Exercises/Chp7_Exercise#5.py
@@ -0,0 +1,8 @@
+# Use def to define describe_city... 
+def describe_city(city, country='Japan'):
+    print(f"{city} is in {country}.")
+
+# Call the function...
+describe_city ("Reykjavik", "Iceland")
+describe_city ("New York", "USA")
+describe_city ("Tokyo")
