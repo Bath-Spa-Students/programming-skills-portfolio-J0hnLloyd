04d603e96d9dab1203f521301bc4706221c480da
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:56:36 +0400

    Chp5_Exercise#1.py

---
+++ Chapter 5- Dictionaries/Exercises/Chp5_Exercise#1.py
@@ -0,0 +1,11 @@
+# Glossary...
+JJa = {'First_Name' : 'Dhen Mark', 
+      'Last_Name' : 'Labasan' ,
+      'Age' : '18' ,
+      'City' : 'Lucena Quezon'}
+
+# Print...
+print (JJa['First_Name'])
+print (JJa['Last_Name'])
+print (JJa['Age'])
+print (JJa['City'])
