9cccdf78dab6118cf154a8edc0985c25542b5ab7
John Lloyd Encenarez Madridano <144320086+J0hnLloyd@users.noreply.github.com>
2023-11-17 18:57:42 +0400

    Chp7_Exercise#1.py

---
+++ Chapter 7- Functions/Exercises/Chp7_Exercise#1.py
@@ -0,0 +1,6 @@
+# Use def to define display_function then add a message... 
+def display_message():
+    print("Today i'm Learning about Python Programming: Functions.")
+
+# Call the function to display the message
+display_message()
